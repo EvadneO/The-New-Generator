6894d205f4fe85c29b06ce36fcb44420d7db0277
EvadneO <evadne28@gmail.com>
2022-03-05 23:08:23 +0000

    The New Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let primero = Math.floor(Math.random() * who.length);
+  let segundo = Math.floor(Math.random() * action.length);
+  let tercero = Math.floor(Math.random() * what.length);
+  let cuarto = Math.floor(Math.random() * when.length);
+
+  document.getElementById("excuse").innerHTML =
+    who[primero] + action[segundo] + what[tercero] + when[cuarto];
   console.log("Hello Rigo from the console!");
 };
